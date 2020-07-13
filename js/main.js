6140b381d0782becd23c13c7d6737989746045bb
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-07-13 14:07:55 +0200

    Clean, sorry for the delay

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
 'use strict';
 
-//R E C U E R D A !: Pintar  > Escuchar > Guardar (LOCALSTORAGE)
-
-//Arrays donde guardaremos el listado de series y las marcadas como favoritas
 let results = [];
 let favourites = [];
 
 //Constantes
 const button = document.querySelector('.js-search-button');
-// const inputValue = document.querySelector('.input').value;
 const favouritesContainer = document.querySelector('.favourite-list-container');
 const resultsContainer = document.querySelector('.results-list-container');
+const resetButton = document.querySelector('.js-reset-button');
 
-//Función que busca las series en el API (API request)
+//1.API request
 function getShows() {
   event.preventDefault();
   const inputValue = document.querySelector('.input').value;
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
-      results = data; //aquí me estoy guardando toda la info del array
+      results = data;
       console.log(results);
       renderShows();
     });
@@ -27,73 +24,125 @@ function getShows() {
 
 button.addEventListener('click', getShows);
 
-//Pinto la información que me devuelve el API en el DOM
+//2.PINTO LA INFO QUE ME TRAIGO DEL API en el DOM
 const showsList = document.querySelector('.js-results-list');
 function renderShows() {
-  console.log(results);
-  //RESET SEARCH
-
+  //reset
   showsList.innerHTML = '';
+  const resultsList = document.querySelector('.js-results-list');
+  const resultsListTitle = document.querySelector('.results-list-title');
+  resultsListTitle.classList.remove('hidden');
   for (let result of results) {
-    // console.log(result.show.name);
-    //añado los li (container)
     const newLi = document.createElement('li');
     showsList.appendChild(newLi);
-    newLi.setAttribute('id', '.js-showItem');
+    newLi.classList.add('js-showItem');
     newLi.setAttribute('id', result.show.id);
-    //añado la imagen
     const newImage = document.createElement('img');
     insertImage(result, newLi);
-    // console.log(result.show.image);
     const newTitle = document.createElement('h2');
     newLi.appendChild(newTitle);
     newTitle.innerHTML = result.show.name;
   }
+  listenRenderShows();
 }
 
-// Función para añadir las imágenes de los respectivos <li>
+// FUNCIÓN PARA AÑADIR IMAGEN
 function insertImage(result, newLi) {
   const newImage = document.createElement('img');
-  newLi.appendChild(newImage); //he creado una función para coger las imágenes del servidor correspondientes a cada iteración y la llamo dentro de la función renderShows.
-  if (!result.show.image) {
+  newLi.appendChild(newImage);
+  if (result.show.image === null) {
     newImage.src = 'https://via.placeholder.com/210x295/ffffff/666666/?';
   } else {
     newImage.src = result.show.image.medium;
   }
 }
-//Escucho las series encontradas para poder añadirlas a los favoritos
-
-// function listenRenderShows() {
-//   const shows = document.querySelectorAll('.showItem');
-//   for (let index = 0; index < results.length; index++) {
-//     shows[index].listenRenderShows('click', saveFavourites);
-//   }
-//   listenRenderShows();
-// }
 
 function listenRenderShows() {
   const shows = document.querySelectorAll('.js-showItem');
-  for (const showItem of showList) {
+  for (const showItem of shows) {
     showItem.addEventListener('click', saveFavourites);
   }
 }
-// listenRenderShows();
-// const handlerShowsClick = (event) => {
-//   const clickedId = parseInt(event.currentTarget.id);
-//   const ShowFind = searchShows.find(
-//     (showItem) => showItem.show.id === clickedId
-//   );
-// };
-// debugger;
-// function saveFavourites(event) {
-//   //identifico el elemento clicado
-//   const clickedShowItemId = event.currentTarget.id;
-//   const showItem = document.geElementById(clickedShowItemId);
-//   showItem.classList.toggle('.favouriteShow');
-//   const favouriteShowItem = results.find((result) => {
-//     return result.show.id === parseInt(showItem.id);
-//   });
 
-//pusheo para agregar los item al final de array
+function saveFavourites(event) {
+  const clickedShowItemId = event.currentTarget.id;
+  const showItem = document.getElementById(clickedShowItemId);
+  showItem.classList.toggle('favouriteShow');
+  const favouriteShowItem = results.find((result) => {
+    return result.show.id === parseInt(showItem.id);
+  });
+  favourites.push(favouriteShowItem);
+  updateLocalStorage();
+  renderFavourites();
+  console.log(favourites);
+}
 
-// favourites.push(favouriteShowItem);
+//PINTO LOS FAVORITOS EN EL DOM
+function renderFavourites() {
+  //Reset
+  const favouritesList = document.querySelector('.favourites-list');
+  favouritesList.innerHTML = '';
+  const favouritesListTitle = document.querySelector('.favourites-list-title');
+  favouritesListTitle.classList.remove('hidden');
+  for (let favourite of favourites) {
+    const newLi = document.createElement('li');
+    favouritesListTitle.appendChild(newLi);
+    newLi.classList.add('js-favouriteItem');
+    newLi.setAttribute('id', favourite.show.id);
+    const newFavouriteImage = document.createElement('img');
+    insertFavouriteImage(favourite, newLi);
+    console.log(favourite.show.image);
+    const newTitle = document.createElement('h2');
+    newLi.appendChild(newTitle);
+    newTitle.innerHTML = favourite.show.name;
+    const removeFavouriteButton = document.createElement('i');
+    removeFavouriteButton.addEventListener('click', removeFavourite);
+
+    //NECESITO MOFICICAR AQUÍ
+
+    //   const favouriteShowItem = results.find((result) => {
+    //     return result.show.id === parseInt(showItem.id);
+    //   });
+    //   favourites.push(favouriteShowItem);
+    //   updateLocalStorage();
+    //   renderFavourites();
+    //   //pusheo para agregar los item al final de array
+    //   console.log(favourites);
+    // }
+    newLi.appendChild(removeFavouriteButton);
+    removeFavouriteButton.classList.add(
+      'removeFavouriteButton',
+      'js-remove-favourite-button'
+    );
+    removeFavouriteButton.innerHTML = 'X';
+  }
+}
+// FUNCIÓN PARA AÑADIR IMAGEN
+function insertFavouriteImage(favourites, newLi) {
+  const newFavouriteImage = document.createElement('img');
+  newLi.appendChild(newFavouriteImage);
+  if (favourites.show.image === null) {
+    newFavouriteImage.src =
+      'https://via.placeholder.com/210x295/ffffff/666666/?';
+  } else {
+    newFavouriteImage.src = favourites.show.image.medium;
+  }
+}
+
+//LOCAL STORAGE
+const updateLocalStorage = () => {
+  localStorage.setItem('favourites', JSON.stringify(favourites));
+};
+const getlFromDataStorage = () => {
+  const dataFromDataStorage = JSON.parse(localStorage.getItem('favourites'));
+  if (favourites !== null) {
+    favourites = dataFromDataStorage;
+  }
+};
+
+//BORRO LOS FAVORITOS
+const removeFavourite = () => {
+  console.log('la primera temporada estuvo mejor');
+};
+getlFromDataStorage();
+renderFavourites();
